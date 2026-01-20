1d6a543c566f70d0454671681c813325963c22fb
bmwoodruff <bmwoodruff@gmail.com>
2026-01-20 13:52:02 -0700

    01-20

---
+++ 01-20.R
@@ -0,0 +1,12 @@
+k <- seq(4,8)
+5-k #checks if it does what we think
+sum(5-k)
+#Doable, but Ben and Chaz agree it's gross.
+sum(5-4:8)
+
+n <- seq(1,4)
+prod(n)
+
+i <- seq(3,7)
+2+i^2
+1/3*sum(2+i^2)
